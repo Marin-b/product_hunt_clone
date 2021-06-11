62428294b812c04c00d2ef6178b38c00c0ae1f34
Marin Bibot <marin.bibot@hotmail.be>
2021-06-11 10:06:48 +0200

    Github action setup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,9 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "#fullname" do
+    user = User.new(first_name: "Bob", last_name: 'Marley')
+    assert_equal user.full_name, "Bob Marley"
+  end
 end
